c450afa95bf2d7efb18deeba3ef0f6943de5b58a
Naomi <naomi@benchling.com>
2021-12-02 13:28:44 -0800

    Day 2 part 2

--- day2.py
+++ day2.py
@@ -4,8 +4,7 @@ def get_instructions():
     file1 = open(data_file_path, 'r')
     return file1.readlines()
 
-if __name__ == "__main__":
-    instructions = [d.strip().split(' ') for d in get_instructions()]
+def calculate_position(instructions):
     horizontal_pos = 0
     depth = 0
     for instruction in instructions:
@@ -17,4 +16,25 @@ if __name__ == "__main__":
             depth = depth - magnitude
         else:
             depth = depth + magnitude
-    print(horizontal_pos * depth)
+    return horizontal_pos * depth
+
+def calculate_pos_with_aim(instructions):
+    horizontal_pos = 0
+    depth = 0
+    aim = 0
+    for instruction in instructions:
+        direction = instruction[0]
+        magnitude = int(instruction[1])
+        if direction == 'forward':
+            horizontal_pos = horizontal_pos + magnitude
+            depth = depth + aim * magnitude
+        elif direction == 'up':
+            aim = aim - magnitude
+        else:
+            aim = aim + magnitude
+    return horizontal_pos * depth
+
+if __name__ == "__main__":
+    instructions = [d.strip().split(' ') for d in get_instructions()]
+    print(calculate_position(instructions))
+    print(calculate_pos_with_aim(instructions))
